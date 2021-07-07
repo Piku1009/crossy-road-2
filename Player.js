f5307792df7d3ccb173687885e7f458fd2bbf2e9
Piku1009 <86112503+Piku1009@users.noreply.github.com>
2021-07-07 15:44:11 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(){
+        this.spt = createSprite(width/2,height+50,30,30);
+        this.spt.shapeColor = "red";
+    }
+
+    moveLeft(){
+        this.spt.x=this.spt.x-10;
+    }
+    moveRight(){
+        this.spt.x=this.spt.x+10;
+    }
+    moveUp(){
+        this.spt.y=this.spt.y-10;
+    }
+    moveDown(){
+        this.spt.y=this.spt.y+10;
+    }
+    
+
+
+}
+
